bfe0f28ff164d9c56ce7ddec9989bfd6eb6b9303
4drea4 <bakerand@outlook.com>
2025-12-09 13:01:32 -0600

    I am working on passing a props property into the value of the usestate

--- task-manager/src/components/TaskFilter/TaskFilter.tsx
+++ task-manager/src/components/TaskFilter/TaskFilter.tsx
@@ -1,17 +1,22 @@
 import React, {useState} from "react";
 import type { TaskFilterProps } from "../../types";
 import type { TaskStatus } from "../../types";
+import {Task} from  "../../types";
 
 export const TaskFilter: React.FC<TaskFilterProps> = ({onFilterChange}) => {
-const [filter, setFilter] = useState<TaskStatus>('Medium');
-const handleFilterChange = (event: React.ChangeEvent<>)
-}
+    //set the status (pending, in progress and completed)
+const [status, setStatus]  =  useState<TaskStatus>;
+const [priority, setPriority] = useState<{Task.priority}>
+
+const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatus(event.target.value);
+};
 
 
     return(
     <div className="filter"> 
     <label>Select Status:</label>
-        <select className="StatusDropdown">
+        <select className="Priorityropdown">
                 <option value="Low">Low</option>
                 <option value="Medium">Medium</option>
                 <option value="High">High</option>
