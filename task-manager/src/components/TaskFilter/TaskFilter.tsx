94056edef53ba41b6a3abfcf2c52b335a9a43773
4drea4 <bakerand@outlook.com>
2025-12-09 12:43:15 -0600

    creating taskfilter component

--- task-manager/src/components/TaskFilter/TaskFilter.tsx
+++ task-manager/src/components/TaskFilter/TaskFilter.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import type { TaskFilterProps } from "../../types";
 import type { TaskStatus } from "../../types";
 
-export function TaskFilter(props: TaskFilterProps) {
-return(
+export const TaskFilter: React.FC<TaskFilterProps> = ({onFilterChange}) => {
+const [filter, setFilter] = useState<TaskStatus>('Medium');
+const handleFilterChange = (event: React.ChangeEvent<>)
+}
+
+
+    return(
     <div className="filter"> 
-        <select>
-           
+    <label>Select Status:</label>
+        <select className="StatusDropdown">
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
         </select>
+        <p>Selected: {}</p>
 
     </div>
     
 )
-}
+
 
