2c8e8e86a9d9f23c9a7397a330757984fae192b2
4drea4 <bakerand@outlook.com>
2025-12-08 19:19:15 -0600

    created interfaces

--- task-manager/src/types/index.ts
+++ task-manager/src/types/index.ts
@@ -14,16 +14,31 @@ export interface Task {
 //parent must pass in 
 export interface TaskListProps {
     //must be a prop called tasks and it must be an array of objects
+    //has to be a task object from task interface
     tasks : Task [];
-    //function prop,
+    //function prop, the parent that has to give tasklist a function with these requirements so when status changes call on this function 
     onStatusChange : (taskId: string, newStatus: TaskStatus) => void;
+    //function prop: when task is deleted call the function using task id
     onDelete: (taskId: string) => void;
 }
 
-//taskitem component 
+//component that displays individual task information.
+    //props for a taskitem component
 
 export interface TaskItemProps {
-    task: Task;
+    task: Task;//task will receive one item 
+
+    // this function will be passed down from the parent
     onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
+
     onDelete: (taskId:string) => void;
+}
+
+// create a TaskFilter component that allows filtering tasks by status and TaskPriorityChangeEvent
+
+export interface TaskFilterProps{
+    onFilterChange: (filters:{
+        status?: TaskStatus;
+        priority?: 'low' | 'medium' | 'high' ;
+    }) => void;
 }
