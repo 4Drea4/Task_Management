1acff98ff716777860790c1df946fe25fc59d4f9
4drea4 <bakerand@outlook.com>
2025-12-08 20:10:14 -0600

    barebones for task filter

---
+++ task-manager/src/components/TaskFilter/TaskFilter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import type { TaskFilterProps } from "../../types";
+import type { TaskStatus } from "../../types";
+
+export function TaskFilter(props: TaskFilterProps) {
+return(
+    <div className="filter"> 
+        <select>
+           
+        </select>
+
+    </div>
+    
+)
+}
+
