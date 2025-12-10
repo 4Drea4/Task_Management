2829b124c04fa75c14925b728364ceae120b126d
4drea4 <bakerand@outlook.com>
2025-12-09 19:21:13 -0600

    need to test

--- task-manager/src/components/TaskList/TaskList.tsx
+++ task-manager/src/components/TaskList/TaskList.tsx
@@ -1,15 +1,29 @@
 import { TaskItem } from "../TaskItem/TaskItem";
-import { TaskListProps } from "../../types";
+import type { TaskListProps } from "../../types";
+
+
 
 export const TaskList: React.FC<TaskListProps> = (props) => {
     return (
         <div>
-
+            {/* mapping over the tasks array from tasklist props */}
+            {props.tasks.map((taskObj) => {
+            return (
+                <div key={taskObj.id}>
+                    <TaskItem
+                    task={taskObj}
+                    onStatusChange={props.onStatusChange}
+                    onDelete={props.onDelete}
+                    />
+                    </div>
+            );
+            }
+        )}
         </div>
 
 )}
 
 
-  );
-
-}
+    
+    
+    
